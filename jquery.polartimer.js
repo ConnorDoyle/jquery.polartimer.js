7823fa0d99779c5c51d20858bd7e9ba3c0311b09
Connor Doyle <connor@oneorangesoftware.com>
2012-03-27 19:46:58 -0500

    Fixed a namespace pollution issue. Modified start and resume setInterval calls to use the element ID, to facilitate running multiple timers simultaneously. Removed a line or two of dead code.

--- jquery.polartimer.js
+++ jquery.polartimer.js
@@ -1,16 +1,18 @@
 (function($) {
-    var seconds_left;
 
     $.fn.polartimer = function(method) {
         // Method calling logic
-        if(methods[method]) {
+        if (methods[method]) {
             return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
-        } else if( typeof method === 'object' || !method) {
+        }
+        else if (typeof method === 'object' || ! method) {
             return methods.init.apply(this, arguments);
-        } else {
+        }
+        else {
             $.error('Method ' + method + ' does not exist on jQuery.polartimer');
         }
     };
+
     var methods = {
         init : function(options) {
             var state = {
@@ -25,7 +27,7 @@
             return this.each(function() {
                 var $this = $(this);
                 var data = $this.data('polartimer');
-                if(!data) {
+                if (! data) {
                     $this.addClass('polartimer');
                     $this.height($this.width());
                     state.timer = null;
@@ -39,44 +41,45 @@
                 }
             });
         },
+
         stopWatch : function() {
             return this.each(function() {
                 var data = $(this).data('polartimer');
-                if(data) {
+                if (data) {
                     var seconds = (data.timerFinish - (new Date().getTime())) / 1000;
                     if(seconds <= 0) {
                         clearInterval(data.timer);
                         $(this).polartimer('drawTimer', 100);
                         data.callback();
-                    } else {
+                    }
+                    else {
                         var percent = 100 - ((seconds / (data.timerSeconds)) * 100);
                         $(this).polartimer('drawTimer', percent);
                     }
                 }
             });
         },
+
         drawTimer : function(percent) {
             return this.each(function() {
                 $this = $(this);
                 var data = $this.data('polartimer');
-                if(data) {
+                if (data) {
                     var w = data.width;
                     var h = data.height;
                     var cx = w / 2;
 
                     data.raphael.clear();
 
-                    if(percent == 100) {
+                    if (percent == 100) {
                         data.raphael.circle(cx, cx, cx).attr({
                             fill : data.color,
                             stroke : 'none',
                             opacity : data.opacity
                         });
-                    } else {
-                        percent_left = percent;
-
+                    }
+                    else {
                         var theta = data.pi2 * percent / 100 - data.piOver2;
-
                         var x1 = Math.cos(theta) * cx + cx;
                         var y1 = Math.sin(theta) * cx + cx;
 
@@ -96,57 +99,68 @@
                 }
             });
         },
-        pause : function() {
-            return this.each(function() {
-                var data = $(this).data('polartimer');
-                seconds_left = (data.timerFinish - (new Date().getTime())) / 1000;
-                if(data) {
 
-                    clearInterval(data.timer);
-                }
-            });
-        },
-        resume : function() {
-            return this.each(function() {
-                var data = $(this).data('polartimer');
-                if(data) {
-                    clearInterval(data.timer);
-                    data.timerFinish = new Date().getTime() + (seconds_left * 1000);
-                    $(this).polartimer('drawTimer', 0);
-                    data.timer = setInterval("$this.polartimer('stopWatch')", 50);
-                }
-            });
-        },
         start : function() {
             return this.each(function() {
                 var data = $(this).data('polartimer');
-                if(data) {
+                if (data) {
                     clearInterval(data.timer);
                     data.timerFinish = new Date().getTime() + (data.timerSeconds * 1000);
                     $(this).polartimer('drawTimer', 0);
-                    data.timer = setInterval("$this.polartimer('stopWatch')", 50);
+					var id = $this.attr('id');
+					data.timer = (! id || id === "") ?
+						setInterval("$this.polartimer('stopWatch')", 50) :
+						setInterval("$('#"+id+"').polartimer('stopWatch')", 50);
                 }
             });
         },
+
+        pause : function() {
+            return this.each(function() {
+                var data = $(this).data('polartimer');
+                data.secondsLeft = (data.timerFinish - (new Date().getTime())) / 1000;
+                if (data) {
+                    clearInterval(data.timer);
+                }
+            });
+        },
+
+        resume : function() {
+            return this.each(function() {
+                var data = $(this).data('polartimer');
+                if (data) {
+                    clearInterval(data.timer);
+                    data.timerFinish = new Date().getTime() + (data.secondsLeft * 1000);
+                    $(this).polartimer('drawTimer', 0);
+					var id = $this.attr('id');
+					data.timer = (! id || id === "") ?
+						setInterval("$this.polartimer('stopWatch')", 50) :
+						setInterval("$('#"+id+"').polartimer('stopWatch')", 50);
+                }
+            });
+        },
+
         reset : function() {
             return this.each(function() {
                 var data = $(this).data('polartimer');
-                if(data) {
+                if (data) {
                     clearInterval(data.timer);
                     $(this).polartimer('drawTimer', 0);
                 }
             });
         },
+
         destroy : function() {
             return this.each(function() {
                 var $this = $(this);
                 var data = $this.data('polartimer');
-                if(data) {
+                if (data) {
                     clearInterval(data.timer);
                     data.raphael.remove();
                     $this.removeData('polartimer');
                 }
             });
         }
+
     };
 })(jQuery);
